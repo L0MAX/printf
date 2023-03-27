616aa279535843d54efd184335f84125ab74695b
L0MAX <ankahdonatus@gmail.com>
2023-03-27 07:31:12 -0700

    created _printf.c

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
+ * L0MAX
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
