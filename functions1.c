ad7b0a69a2dcff4798e6f77715574020a6d4fa5e
L0MAX <ankahdonatus@gmail.com>
2023-03-27 07:40:08 -0700

    created functions 1

--- functions1.c
+++ functions1.c
@@ -79,6 +79,7 @@ int print_octal(va_list types, char buffer[],
 
 /************** PRINT UNSIGNED NUMBER IN HEXADECIMAL **************/
 /**
+ * L0MAX
  * print_hexadecimal - Prints an unsigned number in hexadecimal notation
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
