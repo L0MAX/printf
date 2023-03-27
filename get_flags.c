504f89a138afed89a6c6c21bbbc2c30b659e68db
L0MAX <ankahdonatus@gmail.com>
2023-03-27 07:48:51 -0700

    created flags .c

--- get_flags.c
+++ get_flags.c
@@ -5,11 +5,12 @@
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
+ * L0MAX
  */
 int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
+	/* 1 2 4 8 16 */
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
@@ -32,4 +33,3 @@ int get_flags(const char *format, int *i)
 
 	return (flags);
 }
-
