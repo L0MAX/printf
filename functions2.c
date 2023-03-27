5c25a5fd8d6f7265a42f38ad36f8dd40e560e407
L0MAX <ankahdonatus@gmail.com>
2023-03-27 07:41:14 -0700

    created funstions2:

--- functions2.c
+++ functions2.c
@@ -2,6 +2,7 @@
 
 /****************** PRINT POINTER ******************/
 /**
+ * L0MAX
  * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
