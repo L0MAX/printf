69e15d214cddd28acf3899bbb7871f0641b1b851
L0MAX <ankahdonatus@gmail.com>
2023-03-27 07:54:24 -0700

    created handle_print

--- handle_print.c
+++ handle_print.c
@@ -1,5 +1,8 @@
 #include "main.h"
 /**
+ *
+ *
+ * L0MAX
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
